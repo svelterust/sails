81098e9abced1d31b63070e63feb7468f2f4bd9c
Knarkzel <knarkzel@gmail.com>
2024-10-16 13:51:18 +0200

    urlFor

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,10 +1,14 @@
-// Support local filesystem and Tigris (TODO)
 export const folder = "storage/";
+
+// Get URL for path
+export function urlFor(path: string): string {
+  return "/" + folder + path;
+}
 
 // Uploads file and returns redirect URL
 export async function upload(path: string, data: Blob): Promise<string> {
   await Bun.write(folder + path, data);
-  return "/" + folder + path;
+  return urlFor(path);
 }
 
 // Downloads file and returns Blob
